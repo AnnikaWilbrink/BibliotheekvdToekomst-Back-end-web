96aa682d4a40968b1c6e27b4ec27a201f304648c
stephenkwan1668 <stephenkwan1668@hotmail.com>
2023-10-10 14:18:46 +0200

    Update UserControllerTest.java
    This is a test to check the correctness of the CI pipeline

--- src/test/java/nl/workingtalent/wtlibrary/unit/controller/UserControllerTest.java
+++ src/test/java/nl/workingtalent/wtlibrary/unit/controller/UserControllerTest.java
@@ -2,6 +2,8 @@ package nl.workingtalent.wtlibrary.unit.controller;
 
 import nl.workingtalent.wtlibrary.controller.UserController;
 import nl.workingtalent.wtlibrary.dto.SaveUserDto;
+import nl.workingtalent.wtlibrary.dto.UserDto;
+import nl.workingtalent.wtlibrary.model.User;
 import nl.workingtalent.wtlibrary.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -10,6 +12,8 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.ResponseEntity;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+
+import java.util.Optional;
 
 public class UserControllerTest {
 
@@ -31,5 +35,36 @@ public class UserControllerTest {
 
         assertEquals(200, response.getStatusCodeValue());
         assertTrue(response.getBody());
+    }
+    
+    @Test
+    public void testFindById() {
+        long userId = 1L;
+        User user = new User();
+        user.setId(userId);
+        user.setEmail("test.email@example.com");
+        // ... set other fields ...
+
+        when(userService.findById(userId)).thenReturn(Optional.of(user));
+
+        Optional<UserDto> responseDto = userController.findById(userId);
+        assertTrue(responseDto.isPresent());
+        assertEquals(userId, responseDto.get().getId());
+        assertEquals("test.email@example.com", responseDto.get().getEmail());
+
+        verify(userService, times(1)).findById(userId);
+    }
+    
+    @Test
+    public void testDeleteUser() {
+        long userId = 1L;
+
+        doNothing().when(userService).delete(userId); // Mocking the delete method of UserService
+
+        boolean response = userController.delete(userId);
+        assertTrue(response);
+
+        verify(userService, times(1)).delete(userId);
+        assertFalse(true);
     }
 }
