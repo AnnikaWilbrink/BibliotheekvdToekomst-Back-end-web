63c2043107146e4386460c848a66aca7822fafd5
svenschoonebeek <sven.schoonebeek@ziggo.nl>
2023-10-24 10:46:30 +0200

    Update IBookRepository.java

--- src/main/java/nl/workingtalent/wtlibrary/repository/IBookRepository.java
+++ src/main/java/nl/workingtalent/wtlibrary/repository/IBookRepository.java
@@ -10,5 +10,4 @@ import nl.workingtalent.wtlibrary.model.Book;
 public interface IBookRepository extends JpaRepository<Book, Long>{
 
 	List<Book> findByTitleContainingOrAuthorContainingOrIsbn(String title, String author, String isbn, Sort sort);
-
 }
