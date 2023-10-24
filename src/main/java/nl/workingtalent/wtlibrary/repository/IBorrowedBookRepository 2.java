5d61919be8f1acdfee766ffc7c34e6c0ba98a836
Raoul Schipper <raoulrsd@gmail.com>
2023-10-24 13:08:45 +0200

    duplicate borrowedBook classes

--- src/main/java/nl/workingtalent/wtlibrary/repository/IBorrowedBookRepository 2.java
+++
@@ -1,8 +0,0 @@
-package nl.workingtalent.wtlibrary.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import nl.workingtalent.wtlibrary.model.BorrowedBook; 
-
-public interface IBorrowedBookRepository extends JpaRepository<BorrowedBook,Long> {
-}
