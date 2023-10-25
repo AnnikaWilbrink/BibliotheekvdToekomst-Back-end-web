ba33d378c53907a5c58570909c043c147b5feadc
svenschoonebeek <sven.schoonebeek@ziggo.nl>
2023-10-25 20:02:59 +0200

    email func working

--- src/main/java/nl/workingtalent/wtlibrary/controller/EmailController.java
+++ src/main/java/nl/workingtalent/wtlibrary/controller/EmailController.java
@@ -1,4 +1,5 @@
 package nl.workingtalent.wtlibrary.controller;
+import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.SimpleMailMessage;
@@ -6,12 +7,18 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import nl.workingtalent.wtlibrary.service.EmailService;
 import nl.workingtalent.wtlibrary.dto.EmailDto;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class EmailController {
 
+    
+
     @Autowired
     private EmailService service;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
 
     /* 
     @GetMapping("/compose")
@@ -27,7 +34,7 @@ public class EmailController {
             mailMessage.setTo(dto.getTo());
             mailMessage.setSubject(dto.getSubject());
             mailMessage.setText(dto.getMessage());
-
+            mailMessage.setFrom(fromEmail);
             service.sendEmail(mailMessage);
 
             return "ok";
