3928eea90675cbf19cd6f212c6ba8f897504b268
NinaWilschut <35292794+NinaWilschut@users.noreply.github.com>
2023-10-26 12:38:36 +0200

    review table

--- src/main/java/nl/workingtalent/wtlibrary/repository/IReviewRepository.java
+++ src/main/java/nl/workingtalent/wtlibrary/repository/IReviewRepository.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import nl.workingtalent.wtlibrary.model.Book;
+
 import nl.workingtalent.wtlibrary.model.Review;
 
 public interface IReviewRepository extends JpaRepository<Review, Long>{
 	
 	List<Review> findByBook(Book book);
+
+	List<Review> findByUserId(Long userId);
 
 }
